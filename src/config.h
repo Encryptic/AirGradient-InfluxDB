daf50511780d65c15891c8462e691e671a9df043
Chris Theberge <chris@codeforyou.com>
2023-08-18 18:42:02 -0400

    Upgrades

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 // Enables Root Certificate validation based on the `ISRG Root X1`, this will work with InfluxDB Cloud 
 // and self-hosted servers if you opt for Let's Encrypt as your certificate provider
 //
-//#define USE_IRSG_ROOT_CERT
+#define USE_IRSG_ROOT_CERT
 
 //
 // Enable HTTP Connection Reuse -- Only enable this if you're trying to reduce conneciton overhead
